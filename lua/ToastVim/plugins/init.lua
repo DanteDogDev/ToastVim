e78547227517d0abb62d37ad99d5bdd112973d49
Dante Harper <dantedigdog@gmail.com>
2025-04-18 19:43:12 +0200

    added snacks.nvim

--- lua/ToastVim/plugins/init.lua
+++ lua/ToastVim/plugins/init.lua
@@ -1,6 +1,14 @@
 return {
+{
   "DanteDogDev/ToastVim", 
   opts = {},
   priority = 1000,
   lazy = false,
+},
+{
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {},
+}
 }
