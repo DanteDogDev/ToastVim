89e301e4b3d80e9d8cfdd7c915dc96bee96f5d4b
Dante Harper <dantedigdog@gmail.com>
2025-04-19 17:36:18 +0200

    linter
    linter
    
    linter
    
    lint
    
    linter

--- lua/ToastVim/config/lsp.lua
+++ lua/ToastVim/config/lsp.lua
@@ -9,16 +9,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if not client then return end
 
+    -- Formatting
     if client:supports_method('textDocument/formatting') then
       vim.keymap.set('n', '<leader>cf', function()
-        require("conform").format({bufnr = args.buf})
+        vim.lsp.buf.format({ bufnr = args.buf })
       end, { buffer = args.buf, desc = 'Format Buffer' })
     end
 
   end,
 })
 
+
 vim.diagnostic.config({
   virtual_text = false,
-  virtual_lines = {current_line = true},
+  virtual_lines = { current_line = true },
 })
---
+++ lua/ToastVim/plugins/linter.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-lint",
+  keys = {
+    { "<leader>cl", function() require("lint").try_lint() end, desc = "Lint Buffer" }
+  },
+  opts = {
+    linters_by_ft = {
+    },
+  },
+  config = function(_, opts)
+    local lint = require("lint")
+    lint.linters_by_ft = opts.linters_by_ft
+
+    -- Linting
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
