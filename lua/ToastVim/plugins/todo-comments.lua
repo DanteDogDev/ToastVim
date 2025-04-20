413c91443d44d35d5c23fb119f23fc74e3fe59c2
Dante Harper <dantedigdog@gmail.com>
2025-04-20 15:36:00 +0200

    todo

--- lua/ToastVim/plugins/todo-comments.lua
+++ lua/ToastVim/plugins/todo-comments.lua
@@ -7,8 +7,8 @@ return {
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    -- { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    -- { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
     -- { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
     -- { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
   },
