8c69c65ea9aa7422e1a4db27a68d9ec683a465ea
Dante Harper <dantedigdog@gmail.com>
2025-04-21 20:07:00 +0200

    neogen

---
+++ lua/ToastVim/plugins/neogen.lua
@@ -0,0 +1,13 @@
+return {
+  "danymat/neogen",
+  cmd = "Neogen",
+  keys = {
+    {
+      "<leader>cn",
+      function()
+        require("neogen").generate()
+      end,
+      desc = "Generate Annotations (Neogen)",
+    },
+  },
+}
