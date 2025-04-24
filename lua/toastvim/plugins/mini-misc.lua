85701d937ab850c0d82dcf7a80ef379255899c62
Dante Harper <dantedigdog@gmail.com>
2025-04-24 22:16:03 +0200

    reduce file count

--- lua/toastvim/plugins/mini-misc.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "echasnovski/mini.misc",
-  -- event = { "BufWritePost", "BufReadPost", "InsertLeave" },
-  event = "VeryLazy",
-  config = function()
-    require("mini.misc").setup_auto_root()
-  end
-}
---
+++ lua/toastvim/plugins/mini.lua
@@ -0,0 +1,49 @@
+return {
+{
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = false,
+      width_focus = 30,
+      width_preview = 30,
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>E",
+      function() require("mini.files").open(vim.uv.cwd(), true) end,
+      desc = "Open mini.files (cwd)",
+    },
+  }
+},
+{
+  "echasnovski/mini.icons",
+  lazy = true,
+  opts = {
+  },
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
+},
+{
+  "echasnovski/mini.move",
+  event = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  opts = {},
+},
+{
+  "echasnovski/mini.misc",
+  -- event = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  event = "VeryLazy",
+  config = function()
+    require("mini.misc").setup_auto_root()
+  end
+},
+}
