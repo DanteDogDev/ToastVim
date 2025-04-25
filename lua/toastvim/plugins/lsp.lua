03b0101156df49430cf0974b43faa712a3e88f58
Dante Harper <dantedigdog@gmail.com>
2025-04-25 15:53:34 +0200

    markview
    a
    
    a
    
    a
    
    a

--- lua/toastvim/plugins/lsp.lua
+++ lua/toastvim/plugins/lsp.lua
@@ -64,6 +64,9 @@ return {
   -- MARKDOWN HTML TYPST
   {
     "OXY2DEV/markview.nvim",
+    dependencies = {
+        "saghen/blink.cmp"
+    },
     ft = { "markdown", "html", "yaml", "typst" },
   },
 
