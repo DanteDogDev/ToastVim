85ea9e43d28dd5806f0a735adc2b0d7cd8a30c84
Dante Harper <dantedigdog@gmail.com>
2025-04-28 19:19:31 +0200

    neovim news keybind & conceal level toggle

--- lua/toastvim/plugins/snacks.lua
+++ lua/toastvim/plugins/snacks.lua
@@ -10,6 +10,21 @@ return {
   },
   keys = {
     { "<leader>gg", function() Snacks.lazygit({ cwd = vim.fn.getcwd() }) end, desc = "Lazygit", },
+    {
+      "<leader>fN",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6, height = 0.6,
+          wo = {
+            spell = false, wrap = false,
+            signcolumn = "yes", statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
+    },
   },
   config = function(_, opts)
     require("snacks").setup(opts)
@@ -18,5 +33,6 @@ return {
     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
     Snacks.toggle.inlay_hints():map("<leader>uh")
     Snacks.toggle.diagnostics():map("<leader>ud")
+    Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
   end,
 }
