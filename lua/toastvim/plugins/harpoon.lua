ffdd9c4239159d4d30d65d5cc6f92fa042ed640f
Dante Harper <dantedigdog@gmail.com>
2025-11-04 11:38:24 +0100

    harpoon with with all number keys

--- lua/toastvim/plugins/harpoon.lua
+++ lua/toastvim/plugins/harpoon.lua
@@ -13,6 +13,12 @@ return {
     { "<M-2>", function() Harpoon:list():select(2) end, desc = "Harpoon 2", },
     { "<M-3>", function() Harpoon:list():select(3) end, desc = "Harpoon 3", },
     { "<M-4>", function() Harpoon:list():select(4) end, desc = "Harpoon 4", },
+    { "<M-5>", function() Harpoon:list():select(5) end, desc = "Harpoon 5", },
+    { "<M-6>", function() Harpoon:list():select(6) end, desc = "Harpoon 6", },
+    { "<M-7>", function() Harpoon:list():select(7) end, desc = "Harpoon 7", },
+    { "<M-8>", function() Harpoon:list():select(8) end, desc = "Harpoon 8", },
+    { "<M-9>", function() Harpoon:list():select(9) end, desc = "Harpoon 9", },
+    { "<M-0>", function() Harpoon:list():select(10) end, desc = "Harpoon 10", },
   },
 
   config = function(_, opts)
