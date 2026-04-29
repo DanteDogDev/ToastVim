ea422198898f9e4f98cf4fbc252fd2d364a309dd
Dante Harper <dantedigdog@gmail.com>
2026-04-29 10:13:28 +0200

    testing tree-sitter fix

--- lua/toastvim/plugins/treesitter.lua
+++ lua/toastvim/plugins/treesitter.lua
@@ -1,35 +1,37 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-	build = ":TSUpdate",
 	---@type TSConfig
 	---@diagnostic disable-next-line: missing-fields
 	opts = {
 		auto_install = true,
 		highlight = { enable = true },
-		ensure_installed = {
-			"vim",
-			"vimdoc",
-			"query",
-			"regex",
-			"bash",
-			"lua",
-			"rust",
-
-			"markdown",
-			"markdown_inline",
-			"html",
-			"typst",
-			"yaml",
-
-			"css",
-			"javascript",
-			"norg",
-			"scss",
-			"svelte",
-			"tsx",
-			"vue",
-		},
+		indent = { enable = true },
+		-- ensure_installed = {
+		-- 	"vim",
+		-- 	"vimdoc",
+		-- 	"query",
+		-- 	"regex",
+		-- 	"bash",
+		-- 	"lua",
+		-- 	"rust",
+		--
+		-- 	"markdown",
+		-- 	"markdown_inline",
+		-- 	"html",
+		-- 	"typst",
+		-- 	"yaml",
+		--
+		-- 	"css",
+		-- 	"javascript",
+		-- 	"norg",
+		-- 	"scss",
+		-- 	"svelte",
+		-- 	"tsx",
+		-- 	"vue",
+		-- },
 	},
 }
